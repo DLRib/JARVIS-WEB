f499ba28c0894842c1c892e8fd8ba97789415734
DLRib <davidlucasrib0@gmail.com>
2022-06-03 13:25:06 -0300

    adicionado parcialmente sistema de chamada

--- renderer.js
+++ renderer.js
@@ -51,3 +51,124 @@ else if (nume == 5){
 function fadeSound(){
     document.getElementById('fadeSound').play();
 }
+
+function abre(){
+    let barraCima = document.getElementById('sinalizador');
+    barraCima.classList.add('abrir')
+}
+
+function fecha(){
+    let barraCima = document.getElementById('sinalizador');
+    barraCima.classList.remove('abrir')
+}
+
+//--------------------------------------------
+
+function reconhecer(){
+
+    // This runs when the speech recognition service starts
+    recognition.onstart = function() {
+        console.log("We are listening. Try speaking into the microphone.");
+    };
+    
+    recognition.onspeechend = function() {
+        // when user is done speaking
+        recognition.stop();
+        console.log('parou')
+    }
+                  
+    // start recognition
+    recognition.start();
+
+    // This runs when the speech recognition service returns result
+    recognition.onresult = function(event) {
+        var transcript = event.results[0][0].transcript;
+        var confidence = event.results[0][0].confidence;
+        palavra = transcript;
+        console.log(transcript)
+    }
+}
+
+function comandos(){
+    console.log("deu tudo certo.");
+    
+}
+
+function atencao(){
+    while (lix == 1){
+        reconhecer()
+        //verificar se existe "jarvis":
+        if(palavra.includes("vis") == true){
+            comandos();
+            lix = 0;
+        }
+        else{
+            lix = 1;
+        }
+    }
+}
+
+function iniciar(){
+    lix = 1;
+    while(1 == 1){
+        atencao()
+    }
+}
+
+//-------------reconhecimento de fala--------------------------
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
+    
+    function gravar(){
+    
+      abre()
+    // This runs when the speech recognition service starts
+    recognition.onstart = function() {
+        console.log("We are listening. Try speaking into the microphone.");
+    };
+    
+    recognition.onspeechend = function() {
+        // when user is done speaking
+        recognition.stop();
+        console.log('parou')
+    }
+                  
+    // start recognition
+    recognition.start();
+
+    // This runs when the speech recognition service returns result
+    recognition.onresult = function(event) {
+        var transcript = event.results[0][0].transcript;
+        var confidence = event.results[0][0].confidence;
+        palavra = transcript;
+        console.log(transcript)
+
+
+        //voz do jarvis
+        console.log('conectado')
+
+        speaks = [{"name": "Clarinha", "lang":"pt-BR"}]
+
+        let fala = palavra;
+        console.log('valor recebido: ' + fala)
+
+        const msg = new SpeechSynthesisUtterance(); // cria um objeto sinterizador da fala
+        console.log('objeto criado')
+
+        // ATRIBUTOS/CARACTERISTICAS  DO OBJETO/VOZ
+
+        msg.volume = 1; //define o volume do áudio (de 0 a 1)
+        msg.rate = 1; // define a velocidade do áudio (0.1 a 1)
+        msg.pitch = 0.5; // define o tom em que o áudio é falado (de 0 a 2)
+        msg.text = fala; //Pega o valor do input e passa para o objeto sintetizar
+
+
+        const voice = speaks[0]; //pegamos o objeto que queremos que seja a nossa fala. [0], pois nosso array speaks só possui uma posição, que é a que queremos (Clarinha pt-BR)
+        voice.voiceURI = voice.name; //voiceURI busca o servidor da voz escolhida, no caso, pt-BR defnido em voice.name
+        msg.lang = voice.lang; // Define o idioma a ser utilizado para a fala, no caso, o que foi definido para voice.lang
+        
+        speechSynthesis.speak(msg); //executa a voz
+        fecha()
+    };
+    
+}
